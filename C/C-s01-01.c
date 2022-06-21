614a512b833ccb8d3390558b9813f5c4099b29c8
Kamil Ziemian <kziemianfvt@gmail.com>
2022-06-21 15:36:27 +0200

    Add files via upload

--- C/C-s01-01.c
+++ C/C-s01-01.c
@@ -1,1 +1,18 @@
-// Just for creating catalogue
+#include <stdio.h>
+
+int main() {
+  int c = 0;
+
+
+  printf("Enter a value: ");
+  c = getchar();
+
+
+  printf("\nYou entered:");
+  putchar(c);
+  printf("\n");
+
+
+
+  return 0;
+}
