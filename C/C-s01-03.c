59e6cd019931d1ed0ca2ab6f01f86de26cf3deb0
Kamil Ziemian <kziemianfvt@gmail.com>
2022-06-21 15:38:32 +0200

    Add files via upload

---
+++ C/C-s01-03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+  bool boolVar1 = true;
+  bool boolVar2 = false;
+
+
+  printf("boolVar1 = %d\n", boolVar1);
+  printf("boolVar2 = %d\n", boolVar2);
+
+
+
+  return 0;
+}
