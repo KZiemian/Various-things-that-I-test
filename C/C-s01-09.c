36c73a4525ffac7bfcdeb8dbe3c1ed6730982a66
Kamil Ziemian <kziemianfvt@gmail.com>
2022-06-21 15:48:14 +0200

    C-s01-09.c

---
+++ C/C-s01-09.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  char str[100];
+  int i = 0;
+
+
+  printf("Enter a value: ");
+  scanf("%s %d", str, &i);
+
+
+  printf("\nYou entered: %s %d\n", str, i);
+
+
+
+  return 0;
+}
