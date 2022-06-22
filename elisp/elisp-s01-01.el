958027694fdf49e5f5fc8d41902d141c07249a55
Kamil Ziemian <kziemianfvt@gmail.com>
2022-06-23 01:22:48 +0200

    Add files via upload

--- elisp/elisp-s01-01.el
+++ elisp/elisp-s01-01.el
@@ -1,1 +1,26 @@
-
+(+ 2 2)
+(message "I co wyszlo?")
+`(1
+ 2
+ (
+  3
+  4
+  )
+ )
+(- 2 2 2)
+'a
+'(this is an unquoted list)
+'a
+fill-column
+`(fill-column)
+"abc"
+(concat "abc" "def")
+(substring "The quick brown fox jumped." 16 19)
+(+ 2 fill-column)
+(concat "The " (number-to-string (+ 2 fill-column)) " red foxes.")
+(+)
+(*)
+(+ 3)
+(* 3)
+(+ 2 'hello)
+(zerop 0)
